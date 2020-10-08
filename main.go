1d63d1d9fa20ac27d703a29ad7864062e34cb617
ionut <lupu.ionut@protonmail.ch>
2020-10-08 09:55:27 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "log"
+    "strconv"
+)
+
+func main() {
+    args := os.Args[1:]
+    if len(args) == 0 {
+        log.Fatal("Not enough arguments")
+    }
+    counter, delay := parseArgs(args)
+    fmt.Println(counter, delay)
+}
+
+func parseArgs(args []string) (int, int) {
+    length := len(args)
+    counter := 0
+    delay := 0
+
+    for key, arg := range(args) {
+        switch arg {
+            case "-d":
+                if key+1 < length {
+                    delay, _ = strconv.Atoi(args[key+1])
+                }
+            break;
+
+            case "-c":
+                if key+1 < length {
+                    counter, _ = strconv.Atoi(args[key+1])
+                }
+                break;
+        }
+    }
+    return counter, delay
+}
