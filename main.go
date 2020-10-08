22c620bfb3f2c3b4d90cbaa9732d11bcffbcbb49
ionut <lupu.ionut@protonmail.ch>
2020-10-08 09:58:54 +0300

    code format

--- main.go
+++ main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "log"
-    "strconv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
-    args := os.Args[1:]
-    if len(args) == 0 {
-        log.Fatal("Not enough arguments")
-    }
-    counter, delay := parseArgs(args)
-    fmt.Println(counter, delay)
+	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatal("Not enough arguments")
+	}
+	counter, delay := parseArgs(args)
+	fmt.Println(counter, delay)
 }
 
 func parseArgs(args []string) (int, int) {
-    length := len(args)
-    counter := 0
-    delay := 0
+	length := len(args)
+	counter := 0
+	delay := 0
 
-    for key, arg := range(args) {
-        switch arg {
-            case "-d":
-                if key+1 < length {
-                    delay, _ = strconv.Atoi(args[key+1])
-                }
-            break;
+	for key, arg := range args {
+		switch arg {
+		case "-d":
+			if key+1 < length {
+				delay, _ = strconv.Atoi(args[key+1])
+			}
+			break
 
-            case "-c":
-                if key+1 < length {
-                    counter, _ = strconv.Atoi(args[key+1])
-                }
-                break;
-        }
-    }
-    return counter, delay
+		case "-c":
+			if key+1 < length {
+				counter, _ = strconv.Atoi(args[key+1])
+			}
+			break
+		}
+	}
+	return counter, delay
 }
